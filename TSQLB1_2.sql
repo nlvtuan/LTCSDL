07fbc853029dc64dfadb58b4a4833fc95f4bef6f
lvtuan <tuan.lv@ou.edu.vn>
2017-09-20 10:32:29 +0700

    TSQLB1_2 Cau 1

---
+++ TSQLB1_2.sql
@@ -0,0 +1,26 @@
+﻿USE ONLINE_SHOP
+GO
+
+/*
+ * Câu 1: Viết stored-procedure tính tổng 2 số a, b và in kết quả
+ */
+IF OBJECT_ID('uspCau1', 'P') IS NOT NULL
+	DROP PROC uspCau1
+GO
+ 
+CREATE PROC uspCau1
+	@a float = 0, -- default
+	@b float = 0,
+	@tong float OUT -- OUTPUT
+AS
+	--RETURN (@a + @b)
+	SET @tong = @a + @b;
+GO
+
+-- Gọi
+DECLARE @x float = 3.2, @y float = 5.4, @tong float = 0
+EXEC uspCau1 @x, @y, @tong OUT
+PRINT CAST(@x AS varchar) + ' + ' +
+	CAST(@y AS varchar) + ' = ' +
+	CAST(@tong AS varchar) 
+
